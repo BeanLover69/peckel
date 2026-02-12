6c5c9d023cd3d3942971d7d728ec90eb5cdb8562
EgonDogo <borissopko5@gmail.com>
2026-02-12 11:57:26 +0100

    Implement basic X11 window with event handling
    basics to later on add a dude that you can interact with

---
+++ peckel.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <X11/Xlib.h>
+int main() {
+    bool Running = true;
+    XEvent event;
+    Display* dis = XOpenDisplay(NULL);
+    Atom delete = XInternAtom(dis, "WM_DELETE_WINDOW", False);
+    Window w = XCreateSimpleWindow(dis, XDefaultRootWindow(dis),100,100,100,100, 1, WhitePixel(dis, 0), BlackPixel(dis, 0));
+    XMapWindow(dis, w);
+    unsigned long col = (100,100,100,100);
+    XSetForeground(dis, DefaultGC(dis, 0), WhitePixel(dis, 0));
+    XSelectInput(dis, w, ExposureMask);
+        while(Running) {
+            if (event.type == ClientMessage && event.xclient.data.l[0] == delete)
+            {
+                Running = false;
+                XDestroyWindow(dis, w);
+                return 0;
+            }
+            
+            XNextEvent(dis, &event);
+            if(event.type == Expose) {
+		            XSetForeground(dis, DefaultGC(dis, 0), WhitePixel(dis, 0));
+                XFillRectangle(dis,w,DefaultGC(dis, 0), 10, 10, 100, 100);
+		            XSetForeground(dis,DefaultGC(dis, 0), col);
+		            XFillRectangle(dis,w,DefaultGC(dis, 0), 100,100, 190, 190);
+            }
+        }
+    return 0;
+}
